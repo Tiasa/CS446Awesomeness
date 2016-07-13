a533d7339e949872251005bd430c202a27cd5df3
Lnynx <l.nguyen2606@gmail.com>
2016-07-13 03:45:14 -0400

    Download are now taken course that only available from server

--- NoteBank/app/src/main/java/cs446/notebank/DownloadActivity.java
+++ NoteBank/app/src/main/java/cs446/notebank/DownloadActivity.java
@@ -2,10 +2,14 @@ package cs446.notebank;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.view.View.OnClickListener;
@@ -23,50 +27,107 @@ import org.w3c.dom.Text;
  * Created by aaronlovejan on 6/10/16.OO
  */
 public class DownloadActivity extends Activity {
-    
+    //data is stored in the internal arraylist of data_model
+    //if you want to pull out information simple do this:
+    //              data_model.prof => this return a List<String> with prof
+    public static DataRequest data_model = new DataRequest();
+
     private Spinner sp_course_name,sp_course_id,sp_course_prof;
     Button btnSubmit;
+
+
+
+    //Async task class to get json by making HTTP call
+    //HAVE to create this class because we cant make HTTP request on main thread
+    //any change to the request, URL are done in the: doInBackGround function
+    private class GetData extends AsyncTask<Void, Void, Void> {
+        ProgressDialog pDialog;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(DownloadActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+
+            data_model.getRequest("http://notebank.click/courses");
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+        //create the subclass
+        GetData ct = new GetData();
+        ct.execute();
 
         addItemsOnSpinner();
         addListenerOnButton();
         addListenerOnSpinnerItemSelection();
+
+
     }
 
     // add items into spinner dynamically
     public void addItemsOnSpinner() {
-        // TODO: 6/17/16 The course info should query from server 
-        // TODO: 6/17/16 And the course info should match each other
-        String list_CN[] = {"CS","Math","SE"};
-        String list_CID[] = {"100","200","300"};
-        String list_CProf[] = {"Prof A","Prof B"};
+        // TODO: GET IS DONE - NEED TO DO POST
+
+        //Reading information to the spinner
+        ArrayList<String> list_CN = data_model.course_name;
+        ArrayList<String> list_CID = data_model.course_name;
+        ArrayList<String> list_CProf = data_model.prof;
+
+
         sp_course_name = (Spinner) findViewById(R.id.course_name);
         sp_course_id = (Spinner) findViewById(R.id.course_id);
         sp_course_prof = (Spinner) findViewById(R.id.course_prof);
 
         // generate all drop-down list
-        ArrayAdapter<String>  dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list_CN);
+        ArrayAdapter<String>  dataAdapter = new ArrayAdapter<>(this,
+                                                               android.R.layout.simple_spinner_item,
+                                                               list_CN);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp_course_name.setAdapter(dataAdapter);
 
-        dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list_CID);
+
+
+        dataAdapter = new ArrayAdapter<>(this,
+                                         android.R.layout.simple_spinner_item,
+                                         list_CID);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp_course_id.setAdapter(dataAdapter);
 
-        dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list_CProf);
+
+
+        dataAdapter = new ArrayAdapter<>(this,
+                                         android.R.layout.simple_spinner_item,
+                                         list_CProf);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp_course_prof.setAdapter(dataAdapter);
     }
 
     public void addListenerOnSpinnerItemSelection() {
-        // TODO: 6/17/16 may not need listener for each spinner 
+        // TODO: 6/17/16 may not need listener for each spinner
         sp_course_name = (Spinner) findViewById(R.id.course_name);
         sp_course_name.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
